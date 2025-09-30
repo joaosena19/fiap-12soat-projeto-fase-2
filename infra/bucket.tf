79c74a34a6de0c4a4f5ab4737f72fc556ad160ef
Sena <joaosenadainese@gmail.com>
2025-09-30 20:10:47 -0300

    Teste

--- infra/bucket.tf
+++ infra/bucket.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "bucket_fiap_fase2" {
   bucket = var.bucket_name
-
+TESTE_TERRAFORM_ERRADO
   lifecycle {
     prevent_destroy = true # Como o próprio tfstate está no bucket, previnimos que ele seja destruído acidentalmente
   }
