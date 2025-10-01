1173d3f36a83364cbf063ae5b9104f5e558b6cb5
Sena <joaosenadainese@gmail.com>
2025-09-30 21:36:30 -0300

    Liberar eks

--- infra/access-entry.tf
+++ infra/access-entry.tf
@@ -1,16 +1,16 @@
-# resource "aws_eks_access_entry" "eks_access_entry" {
-#   cluster_name      = aws_eks_cluster.eks_cluster.name
-#   principal_arn     = data.aws_iam_user.iam_user_principal.arn
-#   kubernetes_groups = ["grupo-admins"]
-#   type              = "STANDARD"
-# }
+resource "aws_eks_access_entry" "eks_access_entry" {
+  cluster_name      = aws_eks_cluster.eks_cluster.name
+  principal_arn     = data.aws_iam_user.iam_user_principal.arn
+  kubernetes_groups = ["grupo-admins"]
+  type              = "STANDARD"
+}
 
-# resource "aws_eks_access_policy_association" "eks_access_policy_association" {
-#   cluster_name  = aws_eks_cluster.eks_cluster.name
-#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-#   principal_arn = data.aws_iam_user.iam_user_principal.arn
+resource "aws_eks_access_policy_association" "eks_access_policy_association" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = data.aws_iam_user.iam_user_principal.arn
 
-#   access_scope {
-#     type = "cluster"
-#   }
-# }
+  access_scope {
+    type = "cluster"
+  }
+}
