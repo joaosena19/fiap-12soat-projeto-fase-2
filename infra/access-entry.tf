4090aff66d3f81a6a00113d3478600e8a171a70b
Sena <joaosenadainese@gmail.com>
2025-09-30 22:15:57 -0300

    Teste kubernetes groups

--- infra/access-entry.tf
+++ infra/access-entry.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_access_entry" "eks_access_entry" {
   cluster_name      = aws_eks_cluster.eks_cluster.name
   principal_arn     = data.aws_iam_user.iam_user_principal.arn
-  kubernetes_groups = ["grupo-admins"]
+  kubernetes_groups = ["system:masters"]
   type              = "STANDARD"
 }
 
